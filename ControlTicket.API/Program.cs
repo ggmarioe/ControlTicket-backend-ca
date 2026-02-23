be03a0b0c630980b14abeea82289c58a765ff1d7
marioegg <mario.e.guajardo.g@gmail.com>
2026-02-23 01:26:08 -0300

    Implemented database access configuration

--- ControlTicket.API/Program.cs
+++ ControlTicket.API/Program.cs
@@ -1,8 +1,10 @@
 using ControlTicket.Application;
 using ControlTicket.Infrastructure;
 using ControlTicket.Infrastructure.Logging;
+using ControlTicket.Infrastructure.Persistence;
 using Serilog;
-
+using Pomelo.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 // Bootstrap logger — captures logs during startup before full config is ready
 SerilogConfiguration.CreateBootstrapLogger();
 
@@ -12,7 +14,8 @@ try
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
     var jwtAudience = builder.Configuration.GetSection("Jwt:Audicence").Get<string>() ?? "";
-    var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+    var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>(); 
+    var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
     //var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
     // Configure Serilog from appsettings.json
@@ -25,6 +28,10 @@ try
     builder.Services.AddControllers();
     // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
     builder.Services.AddOpenApi();
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, serverVersion)
+                                                                        .LogTo(Console.WriteLine, LogLevel.Warning));
+
 
    var app = builder.Build();
 
---
+++ ControlTicket.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ControlTicket.Domain.Entities;
+
+namespace ControlTicket.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
