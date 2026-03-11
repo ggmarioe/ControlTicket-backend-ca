1895270038dbcbd095b7068fbdb28331a2409c48
marioegg <mario.e.guajardo.g@gmail.com>
2026-03-11 18:09:48 -0300

    Updated infrastructure layer to add the persistence folder - deleted non vaid DbContext

--- ControlTicket.Infrastructure/Persistence/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-namespace ControlTicket.Infrastructure.Persistence;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
-}
